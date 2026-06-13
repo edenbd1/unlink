d9ab113a11ed9dc5aa2b31ecbb52c97aa9a3fc72
edenbd1 <eden.baudin.invest@gmail.com>
2026-06-13 16:55:24 -0400

    chore(native): APPNAME=UnlinkSB, debug poseidon test proto 192B, ignore bld.txt

--- native/app/src/unlink_crypto.h
+++ native/app/src/unlink_crypto.h
@@ -5,4 +5,4 @@
 // Outputs 32-byte big-endian Ax,Ay (pubkey) and R8x,R8y,S (signature).
 void unlink_sign(const uint8_t *key, size_t keylen, const uint8_t msg_be32[32],
                  uint8_t Ax[32], uint8_t Ay[32], uint8_t R8x[32], uint8_t R8y[32], uint8_t S[32]);
-void unlink_poseidon_test(uint8_t out[32], int nr);
+void unlink_poseidon_test(uint8_t out[192], int nr);
